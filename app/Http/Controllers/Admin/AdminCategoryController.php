1535265acca6acfafaab51d857667f99108124c8
Eddie Luis Rada Gonzalez <eddie.rada@gmail.com>
2020-04-27 15:59:50 -0500

    14 Crear el index de las categorias

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -56,7 +56,7 @@ class AdminCategoryController extends Controller
      */
     public function show($id)
     {
-        //
+       
     }
 
     /**
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,10 +1,77 @@
 @extends('plantilla.admin')
 
-@section('titulo', 'Crear Categoría')
+@section('titulo', 'Adminstración de Categorías')
 
 @section('contenido')
 
-Esto es el index
-{{ dd($categorias) }}
+        <!-- /.row -->
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Sección de Categorías</h3>
+
+                <div class="card-tools">
+                  <div class="input-group input-group-sm" style="width: 150px;">
+                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                    <div class="input-group-append">
+                      <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0" style="height: 400px;">
+               <a class="m-2 float-right btn btn-primary" href="{{ route('admin.category.create') }}">Crear</a>
+                        
+                <table class="table table-head-fixed">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>Slug</th>
+                      <th>Descripción</th>
+                      <th>Fecha de Creación</th>
+                      <th>Fecha de Modificación</th>
+                      <th colspan="3"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    
+                    @foreach($categorias as $categoria)
+                      <tr>
+                        <td> {{ $categoria->id }} </td>
+                        <td> {{ $categoria->nombre }} </td>
+                        <td> {{ $categoria->slug }} </td>
+                        <td> {{ $categoria->description }} </td>
+                        <td><span class="tag tag-success">{{ $categoria->created_at }}</span></td>
+                        <td> {{ $categoria->updated_at }} </td>
+                       
+                        <td> <a class="btn btn-default" href="{{ route('admin.category.show',$categoria->slug) }}">Ver</a>
+                        </td>
+
+                        <td> <a class="btn btn-info" href="{{ route('admin.category.edit',$categoria->slug) }}">Editar</a>
+                        </td>
+
+                        <td> <a class="btn btn-danger" href="{{ route('admin.category.index',$categoria->slug) }}">Eliminar</a>
+                        </td>
+                                          
+                      
+                      </tr>
+                    @endforeach
+                                
+                  
+                    
+                  </tbody>
+                </table>
+                {{ $categorias->links() }}
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- /.row -->
 
 @endsection      
